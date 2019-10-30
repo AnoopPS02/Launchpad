7e3fb45db2a923a1a44733d77b085a6b36ae9497
Anoop P S <38137320+AnoopPS02@users.noreply.github.com>
2019-10-30 23:40:15 +0530

    Update P5.py

--- P5.py
+++ P5.py
@@ -1,3 +1,5 @@
+*program that asks the user for a phrase/sentence & Print back to the user the same string, except with the words in backwards order*
+
 String="My name is Anoop"
 String1=String.split()
 String2=String1[::-1]
